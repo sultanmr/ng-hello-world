23b1af0c5093cc536a083e8639a1b21e712ab5aa
Sultan <sultanmr@hotmail.com>
2017-09-26 22:09:31 +0200

    event filtering

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -5,36 +5,16 @@ import {CoursesService} from '../courses.service';
 @Component({
   selector: 'app-course',
   template: `
-  <h2 [textContent]="title"></h2>
-  <img [src]="imageUrl"/>
-  <table>
-  <tr>
-  <td [attr.colspan]="colspan">
-  </td>
-  </tr>
-  </table>
-  <button class="btn btn-primary" [class.active]="isActive">Class Binding</button>
-  <button [style.backgroundColor]="bkColor">Style Binding</button>
-  <div (click)="onDivClicked()">
-  <button (click)="onEvent($event)">Event Binding</button>
-  </div>
+  <input (keyup.enter)="onKeyUp()" />
+
   `,
   styleUrls: ['./course.component.css']
 })
 
 export class CourseComponent {
-  title = "list of courses";
-  imageUrl = "http://lorempixel.com/400/200"
-  colspan = 2;  
-  isActive = true;
-bkColor = '#ff0000';
 
-  onDivClicked() {
-    console.log("div was clicked");
-  }
+    onKeyUp($event) {
 
-onEvent ($event) {
-    $event.stopPropagation();//stops event bubbling
-    console.log ("button was clicked", $event);
-  }
+        console.log ("enter was pressed");
+    }
 }
