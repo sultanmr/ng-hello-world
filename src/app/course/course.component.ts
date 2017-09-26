12a592e0bcc33ff8443dd0da332d3f0fffa5b3f4
Sultan <sultanmr@hotmail.com>
2017-09-26 21:33:01 +0200

    attribute binding

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -7,16 +7,21 @@ import {CoursesService} from '../courses.service';
   template: `
   <h2 [textContent]="title"></h2>
   <img [src]="imageUrl"/>
+  <table>
+  <tr>
+  <td [attr.colspan]="colspan">
+  </td>
+  </tr>
+  </table>
   `,
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent {
   title = "list of courses";
   imageUrl = "http://lorempixel.com/400/200"
-  
+  colspan = 2;  
 
   constructor () {
 
   }
-
 }
