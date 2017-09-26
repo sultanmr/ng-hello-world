65121d4bc2fe42e15809b74565eeb82e10158b4d
Sultan <sultanmr@hotmail.com>
2017-09-26 22:04:34 +0200

    event binding

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -15,16 +15,26 @@ import {CoursesService} from '../courses.service';
   </table>
   <button class="btn btn-primary" [class.active]="isActive">Class Binding</button>
   <button [style.backgroundColor]="bkColor">Style Binding</button>
+  <div (click)="onDivClicked()">
+  <button (click)="onEvent($event)">Event Binding</button>
+  </div>
   `,
   styleUrls: ['./course.component.css']
 })
+
 export class CourseComponent {
   title = "list of courses";
   imageUrl = "http://lorempixel.com/400/200"
   colspan = 2;  
   isActive = true;
 bkColor = '#ff0000';
-  constructor () {
 
+  onDivClicked() {
+    console.log("div was clicked");
+  }
+
+onEvent ($event) {
+    $event.stopPropagation();//stops event bubbling
+    console.log ("button was clicked", $event);
   }
 }
