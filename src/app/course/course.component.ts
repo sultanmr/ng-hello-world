ff06e3c8e9bb33a3700dd1fc96efc6cd38f9b557
Sultan <sultanmr@hotmail.com>
2017-09-27 01:01:03 +0200

    Pipes

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -6,24 +6,28 @@ import { CoursesService } from '../courses.service';
 @Component({
   selector: 'app-course',
   template: `
-  <input [value]="email1" (keyup.enter)="email1=$event.target.value; onKeyUp1()" />
-  <input [(ngModel)]="email2" (keyup.enter)="onKeyUp2()" />
+  {{course.title | uppercase}} <br/>
+  {{course.students | number}} <br/>
+  {{course.rating | number:'2.2-2'}} <br/> 
+  {{course.price | currency}} <br/>
+  {{course.price | currency:'AUD'}} <br/>
+  {{course.price | currency:'AUD':true}} <br/>
+  {{course.price | currency:'AUD':true:'3.2-2'}} <br/>
+  {{course.releaseDate}} <br/>
+  {{course.releaseDate | date}} <br/>
+  {{course.releaseDate | date:'shortDate'}} <br/>
   `,
   styleUrls: ['./course.component.css']
 })
 
 export class CourseComponent {
 
-  email1="me@example.com";
-  email2="me2@example.com";
-  onKeyUp1() {
-
-    console.log(this.email1);
-  }
-
-  onKeyUp2() {
-    
-        console.log(this.email2);
-      }
+    course = {
+      title: "The Complete Angular Course",
+      rating: 4.2342,
+      students: 23233,
+      price: 190.2334,
+      releaseDate: new Date(2016, 3, 1)
+    }
 
 }
