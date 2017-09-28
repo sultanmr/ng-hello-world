ae61d5995f2011d9f3ec1eec002eb937767ba8e9
Sultan <sultanmr@hotmail.com>
2017-09-28 16:36:27 +0200

    Reusable components events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
+import {FavoriteChangedEventArgs} from './favorite/favorite.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
   post = {
     title: "Title",
     isFavorite: true
+  }
+
+ 
+  onFavoriteChanged (eventArgs:FavoriteChangedEventArgs) {
+    console.log ("fav change ", eventArgs);
   }
 }
--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,22 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css'],
-  inputs: ['isFavorite']
+  styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent implements OnInit {
+  imageSrc1 = "http://via.placeholder.com/150x50/00ff00";
+  imageSrc2 = "http://via.placeholder.com/150x50/0000ff";
+  imageSrc;
+  @Input () isFavorite: boolean;
+  @Output ('clicked') clickedEventEmitter = new EventEmitter();
 
-   isFavorite: boolean;
+  constructor() { 
+    this.setFavImage ();
+  }
 
-  constructor() { }
+  setFavImage () {
+    this.isFavorite ? this.imageSrc = this.imageSrc1 : this.imageSrc=this.imageSrc2;
+  }
 
   ngOnInit() {
   }
 
   onClick () {
     this.isFavorite = !this.isFavorite;
+    this.setFavImage();
+    this.clickedEventEmitter.emit({isFavorite: this.isFavorite});
   }
 
+}
+
+
+export interface FavoriteChangedEventArgs {
+  isFavorite: boolean
 }
