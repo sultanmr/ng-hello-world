6e26ba2985fcf068252f608aa5dbd866674aaf14
Sultan <sultanmr@hotmail.com>
2017-09-30 20:50:33 +0200

    FormGroup, Validators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ContactFormComponent } from './contact-form/contact-form.component';
+import { SignupFormComponent } from './signup-form/Signup-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactFormComponent
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
 
